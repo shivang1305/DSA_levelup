3295280f2b1eb1f4b14bb02fdb1ad763357fb434
shivang1305 <sharmashivang1999@gmail.com>
2021-04-03 23:51:56 +0530

    DP questions added

---
+++ Advanced Dynamic Programming/coin_change_1.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class coin_change_1 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        
+        int[] coins = new int[n];
+        for(int i = 0; i < n; i++)
+            coins[i] = scn.nextInt();
+
+        int sum = scn.nextInt();
+
+        scn.close();
+
+        int[][] dp = new int[n+1][sum+1];
+
+        System.out.println(tabulationSol(coins, sum, n, dp));
+    }
+
+    public static int tabulationSol(int[] arr, int sum, int n, int[][] dp) {
+        for(int i = 0; i < dp.length; i++) {
+            for(int j = 0; j < dp[0].length; j++) {
+                if(i == 0)
+                    dp[i][j] = 0;
+                if(j == 0)
+                    dp[i][j] = 1;
+            }
+        }
+
+        for(int i = 1; i < dp.length; i++) {
+            for(int j = 1; j < dp[0].length; j++) {
+                if(arr[i - 1] <= j)
+                    dp[i][j] = dp[i][j - arr[i - 1]] + dp[i - 1][j];
+                
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+
+        return dp[n][sum];
+    }
+}
---
+++ Advanced Dynamic Programming/count_subsets_with_given_diff.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class count_subsets_with_given_diff {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++)
+            arr[i] = scn.nextInt();
+
+        int diff = scn.nextInt();
+
+        scn.close();
+
+        System.out.println(countSubsetsDiff(arr, diff));
+    }
+
+    public static int countSubsetsDiff(int[] arr, int diff) {
+        int sum = 0, count = 1;
+        for(int val : arr) {
+            sum += val;
+            if(val == 0) // since zero is a special number in case of making subsets as it would not impact the sum of the subset
+                count *= 2;
+        }
+
+        int s1 = (sum + diff) / 2;
+
+        int[][] dp = new int[arr.length + 1][s1 + 1];
+
+        return count * countSubsetSum(arr, arr.length , s1, dp);
+    }
+
+    public static int countSubsetSum(int[] arr, int n, int sum, int[][] dp) {
+        // initialization
+        for(int i = 0; i < dp.length; i++) {
+            for(int j = 0; j < dp[0].length; j++) {
+                if(i == 0)
+                    dp[i][j] = 0;
+                if(j == 0)
+                    dp[i][j] = 1;
+            }
+        }
+
+        for(int i = 1; i < dp.length; i++) {
+            for(int j = 1; j < dp[0].length; j++) {
+                if(arr[i - 1] <= j)
+                    dp[i][j] = dp[i - 1][j - arr[i - 1]] + dp[i - 1][j];
+                
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+
+        return dp[n][sum];
+    }
+}
