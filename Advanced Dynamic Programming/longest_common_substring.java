95def11006f11fbc34dd9cd8c97b912a05bf7601
shivang1305 <sharmashivang1999@gmail.com>
2021-04-10 00:09:15 +0530

    LCS problems added

---
+++ Advanced Dynamic Programming/longest_common_substring.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+public class longest_common_substring {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        String str1 = scn.nextLine();
+        String str2 = scn.nextLine();
+
+        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
+        for(int i = 0; i < dp.length; i++) {
+            for(int j = 0; j < dp[0].length; j++) 
+                dp[i][j] = -1;
+        }
+
+        // System.out.println(lcsRecursive(str1, str2, str1.length(), str2.length()));
+        System.out.println(lcsMemoized(str1, str2, str1.length(), str2.length(), dp, 0));
+        scn.close();
+    }
+
+    public static int lcsRecursive(String X, String Y, int n, int m, int count) { // tle exceeded
+        // BASE CONDITION
+        if(n == 0 || m == 0)
+            return 0;
+
+        if(X.charAt(n - 1) == Y.charAt(m - 1)) 
+            count = lcsRecursive(X, Y, n - 1, m - 1, count + 1);
+
+        else
+            count = Math.max(count, Math.max(lcsRecursive(X, Y, n-1, m, 0), lcsRecursive(X, Y, n, m-1, 0)));
+
+        return count;
+    }
+
+    public static int lcsMemoized(String X, String Y, int n, int m, int[][] dp, int count) { 
+        // BASE CONDITION
+        if(n == 0 || m == 0)
+            return 0;
+
+        if(dp[n][m] != -1)
+            return dp[n][m];
+
+        if(X.charAt(n - 1) == Y.charAt(m - 1)) 
+            return dp[n][m] = lcsRecursive(X, Y, n - 1, m - 1, count + 1);
+        
+
+        else 
+            return dp[n][m] = Math.max(count, Math.max(lcsRecursive(X, Y, n-1, m, 0), lcsRecursive(X, Y, n, m-1, 0)));
+    }
+
+    public static int lcsTabulated(String X, String Y, int n, int m, int[][] dp) { 
+
+        int maxLcs = 0;
+        for(int i = 0; i < dp.length; i++) {
+            for(int j = 0; j < dp[0].length; j++) {
+                if(i == 0 || j == 0) //INITIALIZATION
+                    dp[i][j] = 0;
+                
+                else if(X.charAt(i - 1) == Y.charAt(j - 1)) { //CHOICE DAIGRAM
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    maxLcs = Math.max(maxLcs, dp[i][j]);
+                }
+                
+                else
+                    dp[i][j] = 0;
+            }
+        }
+        return maxLcs;
+    }
+}
+
